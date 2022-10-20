b33df087222c275a6375079598ace64e69ee0052
scare-destiny <zhenya.venger@gmail.com>
2022-10-20 17:01:17 +0300

    image upload

---
+++ graphql/types/Link.ts
@@ -0,0 +1,92 @@
+
+import { User } from './User'
+import { objectType, extendType, intArg, stringArg, nonNull } from 'nexus'
+
+export const Link = objectType({
+  name: 'Link',
+  definition(t) {
+    t.string('id');
+    t.int('index');
+    t.int('userId');
+    t.string('title');
+    t.string('url');
+    t.string('description');
+    t.string('imageUrl');
+    t.string('category');
+  },
+});
+
+export const LinksQuery = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.list.field('links', {
+      type: 'Link',
+      resolve(_parent, _args, ctx) {
+        return ctx.prisma.link.findMany()
+      },
+    })
+  },
+})
+
+
+export const Edge = objectType({
+  name: 'Edge',
+  definition(t) {
+    t.string('cursor')
+    t.field('node', {
+      type: Link,
+    })
+  },
+})
+
+export const PageInfo = objectType({
+  name: 'PageInfo',
+  definition(t) {
+    t.string('endCursor')
+    t.boolean('hasNextPage')
+  },
+})
+
+export const Response = objectType({
+  name: 'Response',
+  definition(t) {
+    t.field('pageInfo', { type: PageInfo })
+    t.list.field('edges', {
+      type: Edge,
+    })
+  },
+})
+
+export const CreateLinkMutation = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('createLink', {
+      type: Link,
+      args: {
+        title: nonNull(stringArg()),
+        url: nonNull(stringArg()),
+        imageUrl: nonNull(stringArg()),
+        category: nonNull(stringArg()),
+        description: nonNull(stringArg()),
+      },
+      async resolve(_parent, args, ctx) {
+
+        if (!ctx.user) {
+          throw new Error(`You need to be logged in to perform an action`)
+        }
+
+        const newLink = {
+          title: args.title,
+          url: args.url,
+          imageUrl: args.imageUrl,
+          category: args.category,
+          description: args.description,
+        }
+
+        return await ctx.prisma.link.create({
+          data: newLink,
+        })
+      },
+    })
+  },
+})
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
-import '../styles/tailwind.css';
-import Layout from '../components/Layout';
+// /pages/_app.tsx
+import '../styles/tailwind.css'
+import Layout from '../components/Layout'
+import { UserProvider } from '@auth0/nextjs-auth0'
+import { ApolloProvider } from '@apollo/client'
+import apolloClient from '../lib/apollo'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </UserProvider>
+  )
 }
 
-export default MyApp;
+export default MyApp
